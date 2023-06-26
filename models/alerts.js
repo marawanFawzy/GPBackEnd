31cdc3f3f048e1188fecf51b4d0e4f5cae342c1f
marawanFawzy <marawan20191700616@cis.asu.edu.eg>
2023-06-26 19:01:07 +0300

    added sql fixed alerts model

--- models/alerts.js
+++ models/alerts.js
@@ -8,7 +8,7 @@ module.exports = class Alert {
         return db.execute('update alerts SET is_read=1 WHERE alert_id =?', [this.id])
     }
     static findAll() {
-        return db.execute('SELECT * FROM alerts')
+        return db.execute('SELECT * FROM eprediction.alerts order by alert_id desc;')
     }
     static findOne(id) {
         return db.execute('SELECT * FROM alerts WHERE alert_id =?', [id])
